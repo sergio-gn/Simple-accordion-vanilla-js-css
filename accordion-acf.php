84dff707b3e2ef7df5ba524be3aca0016f2eaf3c
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-04-17 12:04:41 +1000

    custom fields code to implement the accordion

--- accordion-acf.php
+++ accordion-acf.php
@@ -1,1 +1,16 @@
+<link rel="stylesheet" href="style.css">
 
+<?php if( have_rows('FAQ') ): ?>
+    <div class="accordion v1">
+    <?php while( have_rows('slides') ): the_row(); ?>
+        <div class="a-container">
+            <p class="a-btn"><?php the_sub_field('question'); ?<span></span></p>
+            <div class="a-panel">
+              <p><?php the_sub_field('answer'); ?></p>
+            </div>
+        </div>
+    <?php endwhile; ?>
+    </div>
+<?php endif; ?>
+
+<script src="script.js"></script>
