f9f20d730e9790bf86db25592429eae58df67b0d
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-04-17 12:13:06 +1000

    Update accordion-acf.php

--- accordion-acf.php
+++ accordion-acf.php
@@ -1,10 +1,10 @@
 <link rel="stylesheet" href="style.css">
 
-<?php if( have_rows('FAQ') ): ?>
+<?php if( have_rows('faq') ): ?>
     <div class="accordion v1">
     <?php while( have_rows('slides') ): the_row(); ?>
         <div class="a-container">
-            <p class="a-btn"><?php the_sub_field('question'); ?<span></span></p>
+            <p class="a-btn"><?php the_sub_field('question'); ?><span></span></p>
             <div class="a-panel">
               <p><?php the_sub_field('answer'); ?></p>
             </div>
